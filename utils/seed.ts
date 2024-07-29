c444a0889052971c45f114038e6e47fcc65b03b7
jordyc2163 <jordycaceres00@gmail.com>
2024-07-29 00:10:38 -0500

    updating tab to spaces

--- utils/seed.ts
+++ utils/seed.ts
@@ -7,62 +7,62 @@ const chance = new Chance();
 const prisma = new PrismaClient();
 
 const generateBusinesses = () => {
-	return chance.n(
-		async () => {
-			return await prisma.business.create({
-				data: {
-					id: new ObjectId().toHexString(),
-					name: chance.company(),
-					address: {
-						create: {
-							addressLine1: chance.address(),
-							city: chance.city(),
-							state: chance.state()
-						}
-					}
-				},
-				include: {
-					address: true
-				}
-			});
-		},
-		chance.integer({ min: 2, max: 4 })
-	);
+  return chance.n(
+    async () => {
+      return await prisma.business.create({
+        data: {
+          id: new ObjectId().toHexString(),
+          name: chance.company(),
+          address: {
+            create: {
+              addressLine1: chance.address(),
+              city: chance.city(),
+              state: chance.state()
+            }
+          }
+        },
+        include: {
+          address: true
+        }
+      });
+    },
+    chance.integer({ min: 2, max: 4 })
+  );
 };
 
 const generateUsers = () => {
-	return chance.n(
-		async () => {
-			return await prisma.user.create({
-				data: {
-					id: new ObjectId().toHexString(),
-					email: chance.email(),
-					firstName: chance.name(),
-					lastName: chance.name(),
-					artist: {
-						create: {
-							email: chance.email()
-						}
-					}
-				}
-			});
-		},
-		chance.integer({ min: 2, max: 5 })
-	);
+  return chance.n(
+    async () => {
+      return await prisma.user.create({
+        data: {
+          id: new ObjectId().toHexString(),
+          email: chance.email(),
+          firstName: chance.name(),
+          lastName: chance.name(),
+          artist: {
+            create: {
+              email: chance.email()
+            }
+          }
+        }
+      });
+    },
+    chance.integer({ min: 2, max: 5 })
+  );
 };
 
 const seed = async (): Promise<void> => {
-	try {
-		const businessPromises = generateBusinesses();
-		const userPromises = generateUsers();
+  try {
+    const businessPromises = generateBusinesses();
+    const userPromises = generateUsers();
 
-		await Promise.all(businessPromises);
-		await Promise.all(userPromises);
+    await Promise.all(businessPromises);
+    await Promise.all(userPromises);
 
-		logger.info('Successfully seeded database');
-	} catch (e) {
-		logger.error(`An Error occurred when attempting to seed db: ${e}`);
-	}
+    logger.info('Successfully seeded database');
+  } catch (e) {
+    logger.error(`An Error occurred when attempting to seed db: ${e}`);
+  }
 };
 
 seed();
